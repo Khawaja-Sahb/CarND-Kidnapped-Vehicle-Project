02f482ca51d6c49c6a852dd4ab9b494b798fb0f4
Khawaja-Sahb <63559340+Khawaja-Sahb@users.noreply.github.com>
2020-04-13 02:25:46 +0500

    Update particle_filter.cpp

--- src/particle_filter.cpp
+++ src/particle_filter.cpp
@@ -26,7 +26,7 @@ void ParticleFilter::init(double x, double y, double theta, double std[])
    * NOTE: Consult particle_filter.h for more information about this method 
    *   (and others in this file).
    */
-  num_particles = 20;  // TODO: Set the number of particles
+  num_particles = 200;  // TODO: Set the number of particles
   
   std::normal_distribution<double> distx(x,std[0]);
   std::normal_distribution<double> disty(y,std[1]);
